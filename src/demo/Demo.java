4f1d3e387654a8d88afbaa8b9b93920237485a9e
uo289324 <uo289324@uniovi.es>
2024-09-19 10:12:03 +0200

    cambio en master desde "luna"

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("linea 1");
-		System.out.println("linea 2");
+		System.out.println("cambio en luna");
 		System.out.println("linea 3");
 		
 		//Cambio desde mars
